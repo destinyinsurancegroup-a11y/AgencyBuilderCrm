a40c7c9b19484b13c2529b641c9a688221ffe158
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 15:32:35 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
-<?php
+use App\Http\Controllers\ContactController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn() => view('dashboard'));
-Route::get('/contacts', fn() => view('contacts'));
-Route::get('/book', fn() => view('book'));
-Route::get('/leads', fn() => view('leads'));
-Route::get('/service', fn() => view('service'));
-Route::get('/calendar', fn() => view('calendar'));
-Route::get('/activity', fn() => view('activity'));
-Route::get('/billing', fn() => view('billing'));
-Route::get('/settings', fn() => view('settings'));
-Route::get('/logout', fn() => view('welcome'));
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/contacts',        [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/find',   [ContactController::class, 'find']) ->name('contacts.find'); // JSON
+});
