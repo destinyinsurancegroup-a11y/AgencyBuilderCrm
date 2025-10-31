6589c2cb2791056d20189c95fe95287efcb7875a
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 08:32:41 -0400

    Create dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title','Dashboard')
+@section('content')
+  <div class="section">
+    <h3>Dashboard</h3>
+    <p>Welcome to Agency Builder CRM!</p>
+  </div>
+@endsection
