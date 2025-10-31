e119131547dee9fd292208925e8fe87ee10a6aa0
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 08:33:20 -0400

    Create book.blade.php

---
+++ resources/views/book.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title','Book of Business')
+@section('content')
+  <div class="section">
+    <h3>Book of Business</h3>
+    <p>This will show your client list soon.</p>
+  </div>
+@endsection
