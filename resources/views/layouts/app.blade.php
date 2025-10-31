3714b27493eaa29801cfcef8c2d8474c6fac58e6
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 11:11:47 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-   <link rel="stylesheet" href="{{ url('css/abc.css?v=' . time()) }}">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title', 'Agency Builder CRM')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Agency Builder CRM')</title>
 
-  {{-- Load your black-and-gold CSS file --}}
-  <link rel="stylesheet" href="{{ asset('css/abc.css') }}">
+    {{-- Load the black-and-gold stylesheet securely --}}
+    <link rel="stylesheet" href="{{ secure_asset('css/abc.css') }}">
 </head>
-
 <body>
-  <div class="layout">
-    {{-- Sidebar --}}
-    @include('partials.sidebar')
+    <div class="layout">
+        {{-- Sidebar --}}
+        @include('partials.sidebar')
 
-    {{-- Main Content Area --}}
-    <main class="right">
-      @yield('content')
-    </main>
-  </div>
+        {{-- Main Content Area --}}
+        <main class="right">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
