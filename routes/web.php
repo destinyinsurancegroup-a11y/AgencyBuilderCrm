5b3ccf54eeabd15aa3f37350d587291300b6b637
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 15:45:10 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/contacts',        [ContactController::class, 'index'])->name('contacts.index');
-    Route::get('/contacts/find',   [ContactController::class, 'find']) ->name('contacts.find'); // JSON
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/find', [ContactController::class, 'find'])->name('contacts.find'); // JSON endpoint
 });
