f71eaea5a07ac02671b7747ce7e20dcf54e945e4
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-31 11:16:09 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('dashboard'))->name('dashboard');
-Route::get('/book', fn() => view('book'))->name('book');
-Route::get('/contacts', fn() => view('contacts'))->name('contacts');
-Route::get('/agents', fn() => view('agents'))->name('agents');
-Route::get('/calendar', fn() => view('calendar'))->name('calendar');
-Route::get('/settings', fn() => view('settings'))->name('settings');
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/book', function () {
+    return view('book');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+Route::get('/agents', function () {
+    return view('agents');
+});
+
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+
+Route::get('/settings', function () {
+    return view('settings');
+});
 
 Route::get('/logout', function () {
-    return redirect()->route('dashboard');
-})->name('logout');
+    return redirect('/');
+});
