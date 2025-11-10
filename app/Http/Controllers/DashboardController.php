3e1f5cabb72e1dc3f210f13f035234355954c382
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-10 09:44:11 -0500

    Create DashboardController.php

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    /**
+     * Show the CRM dashboard.
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        return view('dashboard', compact('user'));
+    }
+}
